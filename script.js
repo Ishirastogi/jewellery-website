25ef452a1907069e934c4815d5fe033292e67ef8
Ishi Rastogi <ishi.rastogi@icloud.com>
2024-01-04 21:56:21 +0530

    jewells

---
+++ script.js
@@ -0,0 +1,12 @@
+let productImg= document.getElementById("productImg");
+let btn= document.getElementsByClassName("btn");
+
+btn[0].onclick= function(){
+  productImg.src="anklet.jpeg"
+  
+}
+
+btn[1].onclick= function(){
+productImg.src="ring.jpeg";
+
+}
